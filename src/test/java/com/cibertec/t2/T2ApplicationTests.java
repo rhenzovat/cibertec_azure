881f1c0aa10cdcfee673a5eaa4b91b1a06fe69a1
Javier <javier@runautomat.com>
2025-06-21 02:57:45 -0400

    Feat: Add rest files

---
+++ src/test/java/com/cibertec/t2/T2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
